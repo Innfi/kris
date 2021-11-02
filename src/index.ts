66b480cf58f2aed63737e4cadf3aab094808e5b1
innfi <innfi.world@gmail.com>
2021-11-02 22:06:20 +0900

    swimming hell with es

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('starts');
+import express, { Request, Response } from 'express';
+import fetch from 'node-fetch';
+
+const testUrl = '';
+const testFetch = async (): Promise<unknown> => {
+  const response = await fetch(testUrl);
+  return response.json();
+};
+
+const app = express();
+
+app.get('/', async (_req: Request, res: Response) => {
+  const resp = await testFetch();
+
+  res.status(200).send({
+    err: 'ok',
+    data: resp,
+  }).end();
+});
+
+app.listen(3000, () => {
+  console.log('listen');
+});
