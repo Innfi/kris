ff2e59e537d7e2ab10ce7ba05f26124b7bff02ed
innfi <innfi.world@gmail.com>
2021-12-01 02:40:58 +0900

    have to decide which one is responsible for rawData transformation...

--- src/stat/index.ts
+++ src/stat/index.ts
@@ -1,3 +1,4 @@
 export * from './persistence/adapter.web';
+export * from './persistence/adapter.file';
 export * from './persistence/repository';
 export * from './controller';
--- src/stat/persistence/adapter.file.ts
+++ src/stat/persistence/adapter.file.ts
@@ -3,59 +3,86 @@ import { Service } from 'typedi';
 import { StockData, TimestampTypeEnum } from 'stat/model';
 import fs from 'fs';
 
-
-export interface GetIntradayResult {
+export interface ReadIntradayResult {
   err: string;
   stockData?: StockData;
-};
+}
+
+export interface WriteIntradayResult {
+  err: string;
+}
 
 @Service()
 export class AdapterFile {
-  readonly dataPath='./data/';
+  readonly dataPath = './data/';
 
-  //readIntraday
+  // readIntraday
   async readIntraday(
-    symbol: string, 
-    interval: string
-  ): Promise<GetIntradayResult> {
+    symbol: string,
+    interval: string,
+  ): Promise<ReadIntradayResult> {
     const filename = this.toIntradayFilename(
       TimestampTypeEnum.INTRADAY,
-      symbol, 
-      interval, 
-      );
+      symbol,
+      interval,
+    );
     const effectiveFilePath = `${this.dataPath}${filename}`;
-    if(!fs.existsSync(effectiveFilePath)) {
+    if (!fs.existsSync(effectiveFilePath)) {
       return {
-        err: 'getIntraday] file not found'
+        err: 'file not found',
       };
     }
 
     const rawData: string = fs.readFileSync(effectiveFilePath, 'utf-8');
-    return this.toGetIntradayResult(rawData); 
+    return this.toGetIntradayResult(rawData);
   }
 
   protected toIntradayFilename(
-    type: TimestampTypeEnum, 
-    symbol: string, 
+    type: TimestampTypeEnum,
+    symbol: string,
     interval: string,
   ): string {
     return `${symbol}.${type.toString()}.${interval}.json`;
   }
 
-  protected toGetIntradayResult(rawData: string): GetIntradayResult {    
+  protected toGetIntradayResult(rawData: string): ReadIntradayResult {
     try {
       return {
-        err: 'ok', 
-        stockData: JSON.parse(rawData) as StockData
+        err: 'ok',
+        stockData: JSON.parse(rawData) as StockData,
       };
     } catch (err: any) {
-      //TODO: logging;
+      // TODO: logging;
       return {
-        err: 'parse json failed'
+        err: 'parse json failed',
       };
     }
   }
 
-  //writeIntraday
+  // writeIntraday
+  // suppose i don't need to sync this write call
+  async writeIntraday(
+    symbol: string,
+    interval: string,
+    stockData: StockData,
+  ): Promise<WriteIntradayResult> {
+    const filename = this.toIntradayFilename(
+      TimestampTypeEnum.INTRADAY,
+      symbol,
+      interval,
+    );
+    const effectiveFilePath = `${this.dataPath}${filename}`;
 
-};
+    try {
+      fs.writeFileSync(effectiveFilePath, JSON.stringify(stockData), 'utf8');
+    } catch (err: any) {
+      return {
+        err: 'file write failed',
+      };
+    }
+
+    return {
+      err: 'ok',
+    };
+  }
+}
