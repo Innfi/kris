9af1910c5e2cfebd8f9c858b4d7b2bec36452ef6
innfi <innfi.world@gmail.com>
2022-01-12 21:42:01 +0900

    trady-frontend: fixed settings before applying mui

--- packages/trady-frontend/src/state/reducks.ts
+++ packages/trady-frontend/src/state/reducks.ts
@@ -1,60 +1,73 @@
 import thunk from 'redux-thunk';
 import axios from 'axios';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { History } from 'history';
+// import { History } from 'history';
 
-const backendUrl = 'http://localhost:3000'; //FIXME
+const backendUrl = 'http://localhost:3000'; // FIXME
 
 export interface TradyState {
   email: string;
-  ports: string[]; //FIXME
-  stats: any; //FIXMEEEE
+  ports: string[]; // FIXME
+  stats: any; // FIXMEEEE
 }
 
 interface TradyActionRedux {
   type: string;
-  payload: any;
+  payload?: any;
 }
 
 const STAT_RESP = 'STAT_RESP';
+const ERROR = 'ERROR';
 
-//state
+// state
 const initialState: TradyState = {
   email: '',
   ports: [],
   stats: {},
 };
 
-//reducer
+const initialAction: TradyActionRedux = {
+  type: ERROR,
+};
+
+// reducer
 const tradyReducer = (
   state: TradyState = initialState,
-  action: TradyActionRedux,
+  action: TradyActionRedux = initialAction,
 ): TradyState => {
   switch (action.type) {
-    case STAT_RESP:
-      return {
-        ...state,
-        stats: action.payload.stats,
-      };
-    default:
-      return state;
+  case STAT_RESP:
+    return {
+      ...state,
+      stats: action.payload.stats,
+    };
+  case ERROR:
+    return state;
+  default:
+    return state;
   }
 };
 
 export const rootReducer = combineReducers({ tradyReducer });
 
-//actions
-export const loadStatThunk =
-  (symbol: string) => async (dispatch: Function) => {
+// actions
+export const loadStatThunk = (symbol: string) => {
+  return async (dispatch: Function) => {
     try {
-      const response = await axios.get(backendUrl);
+      const response = await axios.get(`${backendUrl}/${symbol}`);
 
       dispatch({
         type: STAT_RESP,
-        payload: response.data, //FIXME: check data type
+        payload: response.data, // FIXME: check data type
       });
-    } catch (err: unknown) {}
+    } catch (err: unknown) {
+      dispatch({
+        type: ERROR,
+        payload: err,
+      });
+    }
   };
+};
 
-//store
+// store
 export const store = createStore(rootReducer, applyMiddleware(thunk));
