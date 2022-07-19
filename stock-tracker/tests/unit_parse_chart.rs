02b9b9c2c9ba48bf1d92304d1c820071830e7b9c
innfi <innfi.world@gmail.com>
2022-07-19 22:23:16 +0900

    stock-tracker: found ways to get variable key name

--- stock-tracker/tests/unit_parse_chart.rs
+++ stock-tracker/tests/unit_parse_chart.rs
@@ -1,5 +1,6 @@
 use core::panic;
 use std::fs;
+use std::collections::HashMap;
 
 #[test]
 fn load_file() {
@@ -18,6 +19,26 @@ fn json_parse() {
   }
 
   let unwrapped = result.unwrap();
+  assert_eq!(unwrapped["Time Series (Daily)"].is_object(), true);
+
+  unwrapped["Time Series (Daily)"].entries()
+    .for_each(|x: (&str, &json::JsonValue)| {
+    println!("key: {}", x.0);
+    println!("open value: {}", x.1["1. open"]);
+  });
+
   let target_field = &unwrapped["Time Series (Daily)"]["2022-07-13"]["1. open"];
   assert_eq!(target_field.as_str().unwrap(), "34.9500");
+}
+
+#[test]
+fn json_object_dump() {
+  let mut scores = HashMap::new();
+
+  scores.insert(3, "test");
+  scores.insert(2, "this");
+  scores.insert(1, "collection");
+
+  let keys = scores.keys().collect::<Vec<&i32>>();
+  assert_eq!(keys.len(), 3);
 }
