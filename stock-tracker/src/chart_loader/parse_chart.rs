c203fe7ae477d06c4848f9137e435821c9549aed
innfi <innfi.world@gmail.com>
2022-07-20 22:22:15 +0900

    stock-tracker: parse_chart

--- stock-tracker/src/chart_loader/parse_chart.rs
+++ stock-tracker/src/chart_loader/parse_chart.rs
@@ -1,7 +1,32 @@
-// use serde_json::{Result, Value};
+use json;
 
-// pub fn parse_chart(timeseries_key: String, rawData: String) {
+pub struct TimeSeriesUnit {
+  pub date: String,
+  pub chart_value: Vec<String>,
+}
 
+pub fn parse_chart(
+  timeseries_key: String,
+  raw_data: String
+) -> Vec<TimeSeriesUnit> {
+  let name_open = "1.open";
+  let name_high = "2. high";
+  let name_low = "3. low";
+  let name_close = "4. close";
 
-//   todo!()
-// }
+  let parsed = json::parse(&raw_data.as_str()).expect("parse error");
+  let chart_data = &parsed[timeseries_key.as_str()];
+
+  chart_data.entries().map(|x: (&str, &json::JsonValue)| {
+    let mut chart_vec: Vec<String> = Vec::new();
+    chart_vec.push(x.1[name_open].as_str().unwrap().into());
+    chart_vec.push(x.1[name_high].as_str().unwrap().into());
+    chart_vec.push(x.1[name_low].as_str().unwrap().into());
+    chart_vec.push(x.1[name_close].as_str().unwrap().into());
+
+    TimeSeriesUnit {
+      date: x.0.into(),
+      chart_value: chart_vec.clone(),
+    }
+  }).collect()
+}
