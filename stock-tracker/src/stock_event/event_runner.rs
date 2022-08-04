cc3ed64763041fa2399f37dcca952ecaa046bfbf
innfi <innfi.world@gmail.com>
2022-08-04 19:48:44 +0900

    stock-tracker: add a lifetime parameter but it needs testing...

--- stock-tracker/src/stock_event/event_runner.rs
+++ stock-tracker/src/stock_event/event_runner.rs
@@ -1,4 +1,4 @@
-use amiquip::{Connection, Channel, Result, Error, Queue, QueueDeclareOptions};
+use amiquip::{Connection, Channel, Result, Error, Queue, QueueDeclareOptions, ConsumerOptions};
 use log::{error, info};
 
 pub struct EventRunner<'a> {
@@ -16,17 +16,21 @@ impl<'a> EventRunner<'a> {
     }
   }
 
-  pub fn init_channel(&mut self, queue_name: &str) {
+  pub fn init_channel(&'a mut self, queue_name: &str) {
     if self.channel.is_ok() {
       return;
     }
 
     self.channel = self.connection.open_channel(None);
-    self.queue = self.channel.as_ref().unwrap().queue_declare(queue_name,
+    self.queue = self.channel.as_ref::<'a>().unwrap().queue_declare(queue_name,
       QueueDeclareOptions::default());
   }
 
   pub fn try_consume(&mut self) {
-
+    if !self.queue.is_ok() {
+      error!("invalid queue");
+    }
+    let consumer = self.queue.as_ref().unwrap()
+      .consume(ConsumerOptions::default()).expect("consume failed");
   }
 }
