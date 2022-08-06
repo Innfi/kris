919e05e3e87e3399358ab90b8d7d9e07498ab1ad
Innfi <innfi.world@gmail.com>
2022-08-06 22:47:04 +0900

    stock-tracker: modify event handler function

--- stock-tracker/src/stock_event/event_runner.rs
+++ stock-tracker/src/stock_event/event_runner.rs
@@ -1,5 +1,8 @@
-use amiquip::{Connection, Channel, Result, Error, Queue, QueueDeclareOptions, ConsumerOptions};
+use amiquip::{Connection, Channel, Result, Error, Queue, QueueDeclareOptions, ConsumerOptions, ConsumerMessage};
 use log::{error, info};
+use serde_json::from_str;
+
+use super::{EventPayloadTrackStock, handler_track_req};
 
 pub struct EventRunner<'a> {
   connection: Connection,
@@ -9,6 +12,7 @@ pub struct EventRunner<'a> {
 
 impl<'a> EventRunner<'a> {
   pub fn new(url: &str) -> Self {
+    info!("EventRunner.new] ");
     Self { 
       connection: Connection::insecure_open(url).expect("connection error"),
       channel: Err(Error::ClientClosedChannel),
@@ -17,6 +21,7 @@ impl<'a> EventRunner<'a> {
   }
 
   pub fn init_channel(&'a mut self, queue_name: &str) {
+    info!("EventRunner.init_channel] ");
     if self.channel.is_ok() {
       return;
     }
@@ -26,11 +31,33 @@ impl<'a> EventRunner<'a> {
       QueueDeclareOptions::default());
   }
 
-  pub fn try_consume(&mut self) {
+  pub async fn try_consume(&mut self) {
+    info!("EventRunner.try_consume] ");
     if !self.queue.is_ok() {
       error!("invalid queue");
     }
     let consumer = self.queue.as_ref().unwrap()
       .consume(ConsumerOptions::default()).expect("consume failed");
+    for message in consumer.receiver().iter() {
+      match message {
+        ConsumerMessage::Delivery(delivery) => {
+          self.handle_message(&delivery.body).await;
+        }
+        _ => {
+          error!("invalid message")
+        }
+      }
+    }
+
+    // TODO: self.connectin.close()
+  }
+
+  async fn handle_message(&mut self, payload: &Vec<u8>) {
+    info!("EventRunner.handle_message] ");
+    let raw_string = String::from_utf8_lossy(payload);
+    let deserialized: EventPayloadTrackStock = from_str(&raw_string).unwrap();
+
+    let _ = handler_track_req::handle_track_request(deserialized).await;
   }
 }
+
