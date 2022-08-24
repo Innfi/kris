884a73bdcd0084924e31de38969c9ce2880a8078
innfi <innfi.world@gmail.com>
2022-08-24 19:58:15 +0900

    stock-tracker: time to deploy...

--- stock-tracker/src/main.rs
+++ stock-tracker/src/main.rs
@@ -1,6 +1,5 @@
 use futures::join;
 use log::info;
-// use tokio::signal;
 
 use stock_tracker::chart_loader::ChartStorageRedis;
 use stock_tracker::startup::run_http_server;
@@ -12,25 +11,14 @@ async fn main() -> std::io::Result<()> {
   env_logger::init();
 
   info!("start stock_tracker");
-  let http_result = run_http_server();
-
   let storage_redis = ChartStorageRedis::new();
   let mut track_req_handler = TrackRequestHandler::new(&storage_redis);
   let mut event_runner = EventRunnerRabbitMQ::new(&mut track_req_handler);
   let event_runner_result = event_runner.process_event();
 
-  // info!("before signal::ctrl_c()");
-  // match signal::ctrl_c().await {
-  //   Ok(()) => {
-  //     return Ok(());
-  //   },
-  //   Err(err) => {
-  //     error!("failed to wait signal: {}", err);
-  //   },
-  // }
-  // info!("after signal::ctrl_c()");
+  let http_result = run_http_server();
 
-  let _ = join!(http_result, event_runner_result);
+  let _ = join!(event_runner_result, http_result);
 
   Ok(())
 }
--- stock-tracker/src/startup.rs
+++ stock-tracker/src/startup.rs
@@ -1,35 +1,19 @@
-//use actix_web::dev::Server;
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use log::{info, error};
+use log::info;
 use std::net::TcpListener;
-use tokio::signal;
 
 use crate::configuration::CONFS;
 use crate::routes::health_check;
 
-pub async fn run_http_server() -> Result<(), std::io::Error> {
+pub async fn run_http_server() -> Result<Server, std::io::Error> {
   info!("run_http_server");
   let addr = CONFS.healthcheck.addr.as_str();
   let listener = TcpListener::bind(addr)?;
 
-  //let server = 
-  let _ = HttpServer::new(move || {
+  Ok(HttpServer::new(move || {
     App::new().route("/health_check", web::get().to(health_check))
   })
   .listen(listener)?
-  .run();
-
-  match signal::ctrl_c().await {
-    Ok(()) => {
-      info!("received signal");
-      return Ok(());
-    },
-    Err(err) => {
-      error!("signal error: {}", err);
-      return Err(err);
-    },
-  }
-
-  //Ok(server)
-  // Ok(())
+  .run())
 }
